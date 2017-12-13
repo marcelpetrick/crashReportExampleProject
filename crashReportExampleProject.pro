8c3196b30b68e106765e21bfa657216a5642b666
Marcel Petrick <mail@marcelpetrick.it>
2017-12-13 17:57:23 +0100

    trying to add the crash-handler; trying to make it compile and link

--- crashReportExampleProject.pro
+++ crashReportExampleProject.pro
@@ -28,7 +28,8 @@ SOURCES += \
         mainwindow.cpp
 
 HEADERS += \
-        mainwindow.h
+mainwindow.h \
+CrashRpt.h
 
 FORMS += \
         mainwindow.ui
--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+// needed for CrashRpt
+//#include <stdio.h>
+//#include <conio.h>
+//#include <tchar.h>
+#include <assert.h>
+//#include <process.h>
+#include <windows.h>
+//#include <dbghelp.h>
+#include "CrashRpt.h" // Include CrashRpt header
+
 ////for the shortest crashing program :)
 //int main()
 //{
@@ -9,6 +19,30 @@
 
 int main(int argc, char *argv[])
 {
+    // Install crash reporting: taken from the ConsoleDemo.cpp of CrashRpt
+    CR_INSTALL_INFO info;
+    memset(&info, 0, sizeof(CR_INSTALL_INFO));
+    info.cb = sizeof(CR_INSTALL_INFO);             // Size of the structure
+    info.pszAppName = reinterpret_cast<LPCWSTR>("CrashRpt crashReportExampleProject Test"); // App name
+    info.pszAppVersion = reinterpret_cast<LPCWSTR>("1.0.0");              // App version
+    info.pszEmailSubject = reinterpret_cast<LPCWSTR>("CrashRpt crashReportExampleProject Test 1.0.0 Error Report"); // Email subject
+    info.pszEmailTo = reinterpret_cast<LPCWSTR>("mail@marcelpetrick.it");      // Email recipient address
+
+    // Install crash handlers
+    int nInstResult = crInstall(&info);
+    assert(nInstResult==0);
+
+//    // Check result
+//    if(nInstResult!=0)
+//    {
+//        TCHAR buff[256];
+//        crGetLastErrorMsg(buff, 256); // Get last error
+//        _tprintf(_T("%s\n"), buff); // and output it to the screen
+//        return FALSE;
+//    }
+    // end of copy-pasted code
+
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
